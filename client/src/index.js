025a9581d06a565cfd5de1e8cee128822098bc4d
pery mimon <pery.mimon@gmail.com>
2015-05-14 20:30:46 +0300

    add angular-facebook

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,47 @@
 /**
  * Created by pery on 08/05/2015.
  */
-angular.module('Main',['Logs'])
-    .controller('mainController',function($scope,Log,$timeout,$parse){
-            var daysBack = 30;
-            $scope.Log =  Log;
-            $scope.$parse = $parse;
-            Log.updataList(daysBack);
+angular.module('Main',['Logs','facebook'])
+    .config(function(FacebookProvider) {
+    // Set your appId through the setAppId method or
+    // use the shortcut in the initialize method directly.
+        FacebookProvider.init('362389493857685');
+    })
+    .controller('mainController',function($scope,Log,$timeout,$parse,Facebook){
+        $scope.login = function() {
+            // From now on you can use the Facebook service just as Facebook api says
+            Facebook.login(function(response) {
+                console.log(response);
+                // Do something with response.
+            });
+        };
+        $scope.getLoginStatus = function() {
+            Facebook.getLoginStatus(function(response) {
+                if(response.status === 'connected') {
+                    $scope.loggedIn = true;
+                } else {
+                    $scope.loggedIn = false;
+                }
+            });
+        };
 
-            $scope.addLog = function(){
-                Log.addLog($scope.model.log);
-                $scope.model.log = '';
-            };
-        $timeout(function () {
-            $scope.timeNow = moment().format('HH:MM');
+        $scope.me = function() {
+            Facebook.api('/me', function(response) {
+                $scope.user = response;
+                console.log(response);
+            });
+        };
 
-        },1000);
+        var daysBack = 30;
+        $scope.Log =  Log;
+        $scope.$parse = $parse;
+        Log.updataList(daysBack);
+
+        $scope.addLog = function(){
+            Log.addLog($scope.model.log);
+            $scope.model.log = '';
+        };
+
         $scope.moment = moment;
 
     });

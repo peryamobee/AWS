1057f00ad266fc256f3275854f1cb9a2868ac97d
perymimon <pery.mimon@gmail.com>
2015-05-15 01:32:45 +0300

    stable version

--- client/src/index.js
+++ client/src/index.js
@@ -5,17 +5,10 @@ angular.module('Main',['Logs'])
     .controller('mainController',function($scope,Log,$timeout,$parse){
             var daysBack = 30;
             $scope.Log =  Log;
-            $scope.$parse = $parse;
             Log.updataList(daysBack);
 
             $scope.addLog = function(){
                 Log.addLog($scope.model.log);
                 $scope.model.log = '';
             };
-        $timeout(function () {
-            $scope.timeNow = moment().format('HH:MM');
-
-        },1000);
-        $scope.moment = moment;
-
     });

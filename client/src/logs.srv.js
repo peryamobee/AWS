0073f848c86f97b2b4b23ae16e8783e3dd99c416
perymimon <pery.mimon@gmail.com>
2015-05-15 02:31:05 +0300

    code file order

--- client/src/logs.srv.js
+++ client/src/logs.srv.js
@@ -4,56 +4,31 @@
 angular.module('Logs',[])
     .service('Log', function ($http) {
 
-        this.updataList = updataList;
-        this.addLog = addLog;
-
-        Object.defineProperties(this,{
-            list:{
-                get:function(){
-                    return _list;
-                }
-            }
-        });
-
-        var _list = [];
+        this.getLogs = getLogs;
+        this.add = getLogs;
 
         function addLog(logText){
             return $http.post('/log',{
-                text:logText
+                log:logText
             }).then(function (res) {
-                _list.slice(-1)[0].records.push(res.data);
                 return res.data
             })
         }
 
-        function updataList(lastDays){
+        function getLogs(lastDays){
             return $http.get('/log',{
                 params:{
                     lastDays:lastDays
                 }
             }).then(function (res) {
-                angular.copy(res.data,_list) ;
-                var lastDay = _list.slice(-1)[0];
-                var d = lastDay._id;
-                if(!moment().isSame(d.date,'day')){
-                    addThisDay();
-                }
-                //return _list;
+                res.data.forEach(function (day) {
+                    day.records.forEach(function (record) {
+                        record.create = moment(record.create).format('HH:MM');
+                    })
+                });
+                return res.data;
 
 
             })
         }
-        function addThisDay(){
-            var now = moment();
-            _list.push({
-                _id:{
-                    day: now.format('DD'),
-                    month:now.format('MM'),
-                    year: now.format('yyyy')
-                },
-                records:[]
-            })
-        }
-
-
     });
